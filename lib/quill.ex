564bbdd6db60cf996f7d02bd566bb4c121fe7ced
Wibisana Bramawidya <wibisana.bramawidya@gmail.com>
2020-05-18 16:26:52 +0700

    Created base event handler

--- lib/quill.ex
+++ lib/quill.ex
@@ -3,16 +3,41 @@ defmodule Quill do
   Documentation for Quill.
   """
 
-  @doc """
-  Hello world.
+  @behaviour :gen_event
 
-  ## Examples
+  def init(__MODULE__) do
+    # TODO create state from options
+    {:ok, %{}}
+  end
 
-      iex> Quill.hello()
-      :world
+  def handle_call({:configure, _options}, state) do
+    # TODO update state with options
+    {:ok, :ok, state}
+  end
 
-  """
-  def hello do
-    :world
+  def handle_info(_msg, state) do
+    {:ok, state}
+  end
+
+  def terminate(_reason, _state) do
+    :ok
+  end
+
+  def code_change(_old, state, _extra) do
+    {:ok, state}
+  end
+
+
+  def handle_event(:flush, state) do
+    {:ok, state}
+  end
+
+  def handle_event({_, gl, {Logger, _, _, _}}, state) when node(gl) != node() do
+    {:ok, state}
+  end
+
+  def handle_event({_level, _, {Logger, _message, _timestamp, _metadata}}, state) do
+    # TODO output log
+    {:ok, state}
   end
 end

8f523a6590a2955e93d8d040e5938c2790f1e1ba
Wibisana Bramawidya <wibisana.bramawidya@gmail.com>
2020-05-19 15:09:37 +0700

    Prepare for hex publishing

--- mix.exs
+++ mix.exs
@@ -7,16 +7,32 @@ defmodule Quill.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Quill",
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/carash/quill",
+      homepage_url: "https://github.com/carash/quill",
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:jason, "~> 1.2"},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+    ]
+  end
+
+  defp description() do
+    "Simple Json logger backend for Elixir."
+  end
+
+  defp package() do
+    [
+      name: "quill",
+      licenses: ["MIT"],
+      files: ["lib/", "mix.exs", "README.md", "LICENSE"],
+      links: %{"GitHub" => "https://github.com/carash/quill"}
     ]
   end
 end
